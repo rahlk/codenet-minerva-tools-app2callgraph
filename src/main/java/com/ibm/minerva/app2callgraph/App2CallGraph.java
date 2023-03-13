f30bf00dc52d2306e5afc465211c8c386cafb11d
Rahul Krishna <i.m.ralk@gmail.com>
2023-03-13 12:21:24 -0400

    Updated app2call graph to work with JSON

--- src/main/java/com/ibm/minerva/app2callgraph/App2CallGraph.java
+++ src/main/java/com/ibm/minerva/app2callgraph/App2CallGraph.java
@@ -19,41 +19,43 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import org.apache.commons.cli.*;
-
-import com.ibm.wala.ssa.*;
-import com.ibm.wala.classLoader.IClass;
-import com.ibm.wala.classLoader.IMethod;
-import com.ibm.wala.ipa.callgraph.CGNode;
-import com.ibm.wala.ipa.callgraph.CallGraph;
-import com.ibm.wala.ipa.cha.IClassHierarchy;
-import com.ibm.wala.ipa.callgraph.Entrypoint;
-import com.ibm.wala.cast.ir.ssa.AstIRFactory;
-import com.ibm.wala.ipa.callgraph.AnalysisScope;
-import com.ibm.wala.classLoader.CallSiteReference;
-import com.ibm.wala.ipa.callgraph.AnalysisOptions;
-import com.ibm.wala.ipa.cha.ClassHierarchyFactory;
-import com.ibm.wala.ipa.callgraph.CallGraphBuilder;
-import com.ibm.wala.ipa.callgraph.CallGraphBuilderCancelException;
-import com.ibm.wala.ipa.cha.ClassHierarchyException;
-import com.ibm.wala.ipa.callgraph.AnalysisCacheImpl;
-import com.ibm.wala.ipa.callgraph.IAnalysisCacheView;
-import com.ibm.minerva.app2callgraph.utils.AnalysisUtils;
-import com.ibm.wala.cast.java.client.impl.ZeroOneCFABuilderFactory;
-import com.ibm.wala.ipa.callgraph.AnalysisOptions.ReflectionOptions;
-import com.ibm.wala.cast.java.translator.jdt.ecj.ECJClassLoaderFactory;
-// Local Utils
-import com.ibm.minerva.app2callgraph.utils.Log;
-import com.ibm.minerva.app2callgraph.utils.ScopeUtils;
-
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
 // JGraptT to export call graph
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.nio.Attribute;
 import org.jgrapht.nio.DefaultAttribute;
 import org.jgrapht.nio.json.JSONExporter;
+
 import com.ibm.minerva.app2callgraph.entities.CallGraphEdge;
 import com.ibm.minerva.app2callgraph.entities.CallGraphNode;
+import com.ibm.minerva.app2callgraph.utils.AnalysisUtils;
+import com.ibm.minerva.app2callgraph.utils.Log;
+import com.ibm.minerva.app2callgraph.utils.ScopeUtils;
+import com.ibm.wala.cast.ir.ssa.AstIRFactory;
+import com.ibm.wala.cast.java.client.impl.ZeroOneCFABuilderFactory;
+import com.ibm.wala.cast.java.translator.jdt.ecj.ECJClassLoaderFactory;
+import com.ibm.wala.classLoader.CallSiteReference;
+import com.ibm.wala.classLoader.IClass;
+import com.ibm.wala.classLoader.IMethod;
+import com.ibm.wala.ipa.callgraph.AnalysisCacheImpl;
+import com.ibm.wala.ipa.callgraph.AnalysisOptions;
+import com.ibm.wala.ipa.callgraph.AnalysisOptions.ReflectionOptions;
+import com.ibm.wala.ipa.callgraph.AnalysisScope;
+import com.ibm.wala.ipa.callgraph.CGNode;
+import com.ibm.wala.ipa.callgraph.CallGraph;
+import com.ibm.wala.ipa.callgraph.CallGraphBuilder;
+import com.ibm.wala.ipa.callgraph.CallGraphBuilderCancelException;
+import com.ibm.wala.ipa.callgraph.Entrypoint;
+import com.ibm.wala.ipa.callgraph.IAnalysisCacheView;
+import com.ibm.wala.ipa.cha.ClassHierarchyException;
+import com.ibm.wala.ipa.cha.ClassHierarchyFactory;
+import com.ibm.wala.ipa.cha.IClassHierarchy;
+import com.ibm.wala.ssa.SymbolTable;
 
 public class App2CallGraph {
 
@@ -64,7 +66,7 @@ public class App2CallGraph {
    * Convert java binary (*.jar, *.ear, *.war) to a neo4j graph.
    *
    * <p>
-   * usage: ./code2graph [-h|--help] [-q|--quite] [-i|--input <arg> input jar]
+   * usage: ./app2callgraph [-h|--help] [-q|--quite] [-i|--input <arg> input jar]
    * [-d|--outdir <arg>] [-o|--output]
    * output jar]`
    */
@@ -143,6 +145,7 @@ public class App2CallGraph {
       // Build the call graph
       Log.info("Building call graph.");
       long start_time = System.currentTimeMillis();
+      // TODO: Make the context sensitivity configurable
       CallGraphBuilder<?> builder = new ZeroOneCFABuilderFactory().make(options, cache, cha);
       CallGraph callGraph = builder.makeCallGraph(options, null);
       long end_time = System.currentTimeMillis();
@@ -162,6 +165,10 @@ public class App2CallGraph {
     }
   }
 
+  /**
+   * @param klass
+   * @return CallGraphNode
+   */
   private static CallGraphNode buildCallGraphNode(IClass klass) {
     String className = klass.getName().toString();
     Boolean isPrivateClass = klass.isPrivate();
@@ -178,33 +185,45 @@ public class App2CallGraph {
 
   }
 
+  /**
+   * @param callGraph
+   * @return Graph<CallGraphNode, CallGraphEdge>
+   */
   private static Graph<CallGraphNode, CallGraphEdge> getDefaultDirectedGraph(CallGraph callGraph) {
     Graph<CallGraphNode, CallGraphEdge> graph = new DefaultDirectedGraph<>(CallGraphEdge.class);
     for (CGNode entrypointNode : callGraph.getEntrypointNodes()) {
       IMethod entryMethod = entrypointNode.getMethod();
-      // Get call statements that may execute in a given method
+      // Get all callsites that may execute in a given method
       Iterator<CallSiteReference> outGoingCalls = entrypointNode.iterateCallSites();
       for (Iterator<CallSiteReference> it = outGoingCalls; it.hasNext();) {
         CallSiteReference callSiteReference = it.next();
         for (CGNode callTarget : callGraph.getPossibleTargets(entrypointNode, callSiteReference)) {
           if (AnalysisUtils.isApplicationClass(callTarget.getMethod().getDeclaringClass())) {
-
+            // Create a node for the source class
             IClass sourceClass = entryMethod.getDeclaringClass();
             CallGraphNode source = buildCallGraphNode(sourceClass);
 
+            // Create a node for the target class
             IClass targetClass = callTarget.getMethod().getDeclaringClass();
             CallGraphNode target = buildCallGraphNode(targetClass);
 
+            // Ignore self references
             if (source.equals(target)) {
               continue;
             }
+
+            // Add the vertices to the final graph
             graph.addVertex(source);
             graph.addVertex(target);
+            // Get the edge between the source and the traget
             CallGraphEdge cgEdge = graph.getEdge(source, target);
+            // If no edge exists, then create one...
             if (cgEdge == null) {
               CallGraphEdge edge = new CallGraphEdge(entryMethod.getName(), callTarget.getMethod().getName(), 1.0);
               graph.addEdge(source, target, edge);
-            } else {
+            }
+            // If edge exists, then increment the weight
+            else {
               cgEdge.incrementWeight();
             }
           }
@@ -214,12 +233,16 @@ public class App2CallGraph {
     return graph;
   }
 
+  /**
+   * @param callGraph
+   * @param outPath
+   * @param outFile
+   */
   public static void saveCallGraph(CallGraph callGraph, String outPath, String outFile) {
     Graph<CallGraphNode, CallGraphEdge> graph = getDefaultDirectedGraph(callGraph);
     JSONExporter<CallGraphNode, CallGraphEdge> exporter = new JSONExporter<>(v -> v.className);
     exporter.setVertexAttributeProvider((v) -> {
       Map<String, Attribute> map = new LinkedHashMap<>();
-      map.put("label", DefaultAttribute.createAttribute(v.toString()));
       map.put("is_class_private", DefaultAttribute.createAttribute(v.getIsPrivate()));
       map.put("num_total_fields", DefaultAttribute.createAttribute(v.getNum_fields()));
       map.put("num_static_fields", DefaultAttribute.createAttribute(v.getNum_static_fields()));
